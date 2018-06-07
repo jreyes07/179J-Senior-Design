ed492d47988e0509404b625b5ea6eb7e53f2817d
jreyes07 <jreye047@ucr.edu>
2018-06-06 23:54:47 -0700

    Add files via upload

--- Code/Gloves-Slaves/main.c
+++ Code/Gloves-Slaves/main.c
@@ -211,76 +211,77 @@ int SM2_Tick(int state)
 		break;
 		
 		case wait2://wait to receive signal from Central hub that workout is about to begin
-		if(USART_HasReceived(0))
+		if(USART_HasReceived(1))
 		{
-			ON = USART_Receive(0);
+			ON = USART_Receive(1);
 			PORTA = ON;
 			choice = ON;
-			USART_Flush(0);
+			USART_Flush(1);
 		}
 		// 		PORTA = 0x00;
 		break;
 		
 		case send_SB:
 
-		 		if(USART_HasReceived(0))
-		 		{
-		 			ON = USART_Receive(0);
-		 			PORTA = ON;
-		 			USART_Flush(0);
-				}
-		count++;
+		if(USART_HasReceived(1))
+		{
+			ON = USART_Receive(1);
+			PORTA = ON;
+			USART_Flush(1);
+		}
+		
 		if(ON > 0x00)
 		{
 			if(Ya >= 1.1)
 			{
 				unsigned char sending = 0x01;
-				USART_Flush(0);
-				if(USART_IsSendReady(0))
-				USART_Send(sending,0);//send a bit to progress state of exercise
-				while(!USART_HasTransmitted(0))
+				USART_Flush(1);
+				if(USART_IsSendReady(1))
+				USART_Send(sending,1);//send a bit to progress state of exercise
+				while(!USART_HasTransmitted(1))
 				{
 					//wait until transmittedP
-					PORTA = 0x00;
+					// 					PORTA = 0x00;
 				}
-				USART_Flush(0);
+				USART_Flush(1);
+				count++;
 				PORTA = 0x01;
 			}
 		}
-// 		else if (count >=20)
-// 		{
-// 			if(Ya >= 2)
-// 			{
-// 				unsigned char sending = 0x01;
-// 				USART_Flush(0);
-// 				if(USART_IsSendReady(0))
-// 				USART_Send(sending,0);//send a bit to progress state of exercise
-// 				while(!USART_HasTransmitted(0))
-// 				{
-// 					//wait until transmittedP
-// 					PORTA = 0x00;
-// 				}
-// 				
-// 				PORTA = 0x01;
-// 			}
-// 		}
-// 		else if (count >=30)
-// 		{
-// 			if(Ya >= 1 && Za >= 1)
-// 			{
-// 				unsigned char sending = 0x01;
-// 				USART_Flush(0);
-// 				if(USART_IsSendReady(0))
-// 				USART_Send(sending,0);//send a bit to progress state of exercise
-// 				while(!USART_HasTransmitted(0))
-// 				{
-// 					//wait until transmittedP
-// 					PORTA = 0x00;
-// 				}
-// 				
-// 				PORTA = 0x01;
-// 			}
-// 		}
+		// 		else if (count >=20)
+		// 		{
+		// 			if(Ya >= 2)
+		// 			{
+		// 				unsigned char sending = 0x01;
+		// 				USART_Flush(1);
+		// 				if(USART_IsSendReady(1))
+		// 				USART_Send(sending,1);//send a bit to progress state of exercise
+		// 				while(!USART_HasTransmitted(1))
+		// 				{
+		// 					//wait until transmittedP
+		// 					PORTA = 0x00;
+		// 				}
+		// 				count++;
+		// 				PORTA = 0x01;
+		// 			}
+		// 		}
+		// 		else if (count >=30)
+		// 		{
+		// 					if(Ya >= 2)
+		// 					{
+		// 						unsigned char sending = 0x01;
+		// 						USART_Flush(1);
+		// 						if(USART_IsSendReady(1))
+		// 						USART_Send(sending,1);//send a bit to progress state of exercise
+		// 						while(!USART_HasTransmitted(1))
+		// 						{
+		// 							//wait until transmittedP
+		// 							PORTA = 0x00;
+		// 						}
+		// 						count++;
+		// 						PORTA = 0x01;
+		// 					}
+		// 		}
 
 		// 		else if(Za < 0.5)//resting fist is going to be pointed up so Za won't much applied force
 		// 		{
@@ -304,10 +305,10 @@ int SM2_Tick(int state)
 		
 		if(Xa >= 0.4 && Za >= 0.5)
 		{
-			USART_Flush(0);
-			if(USART_IsSendReady(0))
-			USART_Send(0x01,0);//send a bit to progress state of exercise
-			while(!USART_HasTransmitted(0))
+			USART_Flush(1);
+			if(USART_IsSendReady(1))
+			USART_Send(0x01,1);//send a bit to progress state of exercise
+			while(!USART_HasTransmitted(1))
 			{
 				//wait until transmitted
 			}
@@ -315,10 +316,10 @@ int SM2_Tick(int state)
 		}
 		else if(Xa < 0.4)//hands will be moving down for jumping jacks
 		{
-			USART_Flush(0);
-			if(USART_IsSendReady(0))
-			USART_Send(0x00,0);//send a bit to progress state of exercise
-			while(!USART_HasTransmitted(0))
+			USART_Flush(1);
+			if(USART_IsSendReady(1))
+			USART_Send(0x00,1);//send a bit to progress state of exercise
+			while(!USART_HasTransmitted(1))
 			{
 				//wait until transmitted
 			}
@@ -335,10 +336,10 @@ int SM2_Tick(int state)
 		
 		if(Za >= 0.5)//send data once user is in push-up position
 		{
-			USART_Flush(0);
-			if(USART_IsSendReady(0))
-			USART_Send(0x01,0);//send a bit to progress state of exercise
-			while(!USART_HasTransmitted(0))
+			USART_Flush(1);
+			if(USART_IsSendReady(1))
+			USART_Send(0x01,1);//send a bit to progress state of exercise
+			while(!USART_HasTransmitted(1))
 			{
 				//wait until transmitted
 			}
@@ -354,20 +355,20 @@ int SM2_Tick(int state)
 		// 		}
 		if(Za < 0)//send data once user is in sit-up position
 		{
-			USART_Flush(0);
-			if(USART_IsSendReady(0))
-			USART_Send(0x01,0);//send a bit to progress state of exercise
-			while(!USART_HasTransmitted(0))
+			USART_Flush(1);
+			if(USART_IsSendReady(1))
+			USART_Send(0x01,1);//send a bit to progress state of exercise
+			while(!USART_HasTransmitted(1))
 			{
 				//wait until transmitted
 			}
 		}
 		else if(Xa > 0.5)
 		{
-			USART_Flush(0);
-			if(USART_IsSendReady(0))
-			USART_Send(0x00,0);//send a bit to progress state of exercise
-			while(!USART_HasTransmitted(0))
+			USART_Flush(1);
+			if(USART_IsSendReady(1))
+			USART_Send(0x00,1);//send a bit to progress state of exercise
+			while(!USART_HasTransmitted(1))
 			{
 				//wait until transmitted
 			}
@@ -392,8 +393,8 @@ int main()
 	
 	I2C_Init();											/* Initialize I2C */
 	MPU6050_Init();										/* Initialize MPU6050 */
-	initUSART(0);	//0 for right hand					/* Initialize USART with 9600 baud rate */
-	//initUSART(1); //1 for left hand
+	// 	initUSART(0);	//0 for left hand					/* Initialize USART with 9600 baud rate */
+	initUSART(1); //1 for right hand
 	
 	//declare number of tasks using tasksNum
 	tasksNum = 2; //Task 1 can be reading information from Gyroscope
